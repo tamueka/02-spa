be53f2fe67926b593d30dbd22d82256c7b063414
Samuel Ruiz <samuel.ruiz@aktios.com>
2020-02-18 20:29:10 +0100

    usamos output para emitir un evento del componente hijo al componente padre

--- src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
+++ src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router'
 
 @Component({
@@ -11,13 +11,18 @@ export class HeroeTarjetaComponent implements OnInit {
   @Input() heroe: any = {};
   @Input() index: number;
 
-  constructor(private router: Router) { }
+  @Output() heroeSeleccionado: EventEmitter<number>;
+
+  constructor(private router: Router) {
+    this.heroeSeleccionado = new EventEmitter();
+  }
 
   ngOnInit(): void {
   }
 
   verHeroe(){
-    this.router.navigate(['/heroe', this.index])
+    //this.router.navigate(['/heroe', this.index])
+    this.heroeSeleccionado.emit(this.index);
   }
 
 }
